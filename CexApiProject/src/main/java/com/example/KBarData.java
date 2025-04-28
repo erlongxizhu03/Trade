704763ac21468002dfc86bcea4b8f5412177956d
erlongxizhu03 <1593612833@qq.com>
2025-04-28 15:56:15 +0800

    Add files via upload

---
+++ CexApiProject/src/main/java/com/example/KBarData.java
@@ -0,0 +1,64 @@
+package com.example;
+
+/**
+ * @Description TODO
+ * @Author erlong.zhou
+ * @Date 2025/4/28 10:41
+ */
+public class KBarData {
+
+    public String time;
+    public String open;
+    public String hig;
+    public String low;
+    public String close;
+    public String isConfirm;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getOpen() {
+        return open;
+    }
+
+    public void setOpen(String open) {
+        this.open = open;
+    }
+
+    public String getHig() {
+        return hig;
+    }
+
+    public void setHig(String hig) {
+        this.hig = hig;
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+
+    public String getClose() {
+        return close;
+    }
+
+    public void setClose(String close) {
+        this.close = close;
+    }
+
+    public String getIsConfirm() {
+        return isConfirm;
+    }
+
+    public void setIsConfirm(String isConfirm) {
+        this.isConfirm = isConfirm;
+    }
+}
