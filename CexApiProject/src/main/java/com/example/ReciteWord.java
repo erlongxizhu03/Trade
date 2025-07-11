091c5945699f7f253cea1f289900e095af8eaf38
erlongxizhu03 <1593612833@qq.com>
2025-07-11 14:44:55 +0800

    Update ReciteWord.java
    优化

--- CexApiProject/src/main/java/com/example/ReciteWord.java
+++ CexApiProject/src/main/java/com/example/ReciteWord.java
@@ -27,35 +27,35 @@ public class ReciteWord {
             // 正则表达式匹配单词、音标和释义
             Pattern pattern = Pattern.compile("(\\w+)\\s+/([^/]+)/\\s*(\\w*\\.*)(.*?)(?=\\w+\\s+/)", Pattern.DOTALL);
             Matcher matcher = pattern.matcher(sb.toString());
-            Map<String, String> wordMap = new LinkedHashMap<>();
+            Map<String, String> wordPhoneticMap = new LinkedHashMap<>();
+            Map<String, String> wordDefiMap = new HashMap<>();
             while (matcher.find()) {
                 String word = matcher.group(1).trim(); // 单词
                 String phonetic = matcher.group(2).trim(); // 音标
                 String partOfSpeech = matcher.group(3).trim(); // 词性
                 String definition = matcher.group(4).trim(); // 释义
                 // 将单词和释义存入Map中
-                wordMap.put(word+"_"+phonetic, partOfSpeech + definition);
+                wordPhoneticMap.put(word, phonetic);
+                wordDefiMap.put(word, partOfSpeech+definition);
             }
             // 输出结果
             int i = 1;
-            for (Map.Entry<String, String> entry : wordMap.entrySet()) {
-                System.out.println(i++ + ":" + entry.getKey() + "===" + entry.getValue());
+            for (Map.Entry<String, String> entry : wordPhoneticMap.entrySet()) {
+                System.out.println(i++ + ":" + entry.getKey() + "===" + entry.getValue()+wordDefiMap.get(entry.getKey()));
             }
             Scanner scanner = null;
-            System.out.println("\u001B[31m根据释义输入对应的单词:\u001B[0m");
+            System.out.println("\u001B[31m根据音标输入对应的单词:\u001B[0m");
             while (true) {
                 // 随机获取一个键值对(单词和释义
-                String wordPhonetic = getRandomKey(wordMap);
-                String word = wordPhonetic.split("_")[0];
-                String phonetic = wordPhonetic.split("_")[1];
-                String randomValue = wordMap.get(wordPhonetic);
-                System.out.println("释义===" + randomValue + ":");
+                String word = getRandomKey(wordPhoneticMap);
+                String randomValue = wordPhoneticMap.get(word);
+                System.out.println("音标===: /" + randomValue + "/");
 
                 scanner = new Scanner(System.in);
                 String input = scanner.nextLine(); // 读取一行文本
 
                 while (input == null || !input.equals(word.toLowerCase())) {
-                    System.out.println("不正确，请再次输入,音标提示, /"+phonetic+"/:");
+                    System.out.println("\u001B[31m不正确，请再次输入,音标提示, \u001B[0m/"+randomValue+"/:");
                     scanner = new Scanner(System.in);
                     input = scanner.nextLine(); // 读取一行文本
                     if ("1".equals(input)) {
@@ -63,7 +63,7 @@ public class ReciteWord {
                         System.out.println(word);
                     }
                 }
-                System.out.println("正确，请输入下一个：");
+                System.out.println("\u001B[34m正确，"+word+" /"+randomValue+"/ \u001B[0m"+wordDefiMap.get(word)+",下一个：");
             }
         } catch (Exception e) {
             e.printStackTrace();
